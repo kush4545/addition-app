f008d6eda2e53244061fa1518d86b066fb5401cc
kush desai <kush@artiumacademy.com>
2023-06-16 14:25:31 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const handleNumberChange = (e, setNumber) => {
+    setNumber(parseInt(e.target.value, 10));
+  };
+
+  const handleAddition = () => {
+    setResult(number1 + number2);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Addition App</h1>
+      <input
+        type="number"
+        value={number1}
+        onChange={(e) => handleNumberChange(e, setNumber1)}
+      />
+      <input
+        type="number"
+        value={number2}
+        onChange={(e) => handleNumberChange(e, setNumber2)}
+      />
+      <button onClick={handleAddition}>Add</button>
+      <p>Result: {result}</p>
     </div>
   );
 }
